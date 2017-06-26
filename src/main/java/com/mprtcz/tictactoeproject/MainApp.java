5c4e0a347061188a33174a9896ac6d85582e8e01
mprtcz <azet89@gmail.com>
2017-06-26 19:57:18 +0200

    Initial setup continued; Added compiler plugin; Added MainApp stub Added fxml file;

--- src/main/java/com/mprtcz/tictactoeproject/MainApp.java
+++ src/main/java/com/mprtcz/tictactoeproject/MainApp.java
@@ -1,7 +1,37 @@
 package com.mprtcz.tictactoeproject;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Created by azotan on 26.06.17.
  */
-public class MainApp {
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage window) throws Exception{
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/main.fxml"));
+        System.out.println(loader.getLocation());
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root, 1024, 768);
+
+        window.setOnCloseRequest(e -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        window.setTitle("Tic Tac Toe Game");
+        window.setScene(scene);
+        window.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
